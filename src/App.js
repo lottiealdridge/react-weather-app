2c343c6767afab9aed9411b1506ee8a2dd97d718
lottiealdridge <lottiealdridge95@gmail.com>
2025-04-05 09:56:39 +0100

    added github repository link

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <Weather/>
-
-
+      <a href="https://github.com/lottiealdridge/react-weather-app" rel ="noreferrer"target="_blank">See open sourced code here</a>
     </div>
   );
 }
