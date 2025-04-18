c43cc780fffb39e27ff4b5d243b766e86b0a4654
lottiealdridge <lottiealdridge95@gmail.com>
2025-04-18 11:22:41 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <Weather/>
       <a href="https://github.com/lottiealdridge/react-weather-app" rel ="noreferrer"target="_blank">See open sourced code here</a>
+      </div>
     </div>
   );
 }
