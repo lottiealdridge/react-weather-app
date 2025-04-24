2df55a3e64353171ee3aceb567ac0ff15d252f4e
lottiealdridge <lottiealdridge95@gmail.com>
2025-04-24 22:17:15 +0100

    fixed refresh error on 5 day forecast

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,32 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-    const[unit, setUnit] = useState("celsius");
-
-    function showFarenheit(event) {
-        event.preventDefault();
-        setUnit("farenheit");
-    }
-
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    if (unit === "celsius") {
     return (
         <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">℃ | <a href="/" onClick={showFarenheit}>℉</a> </span>
+        <span className="unit">℃ </span>
         </div>
     );
-    } else {
-        let farenheit = (props.celsius * 9) /5 + 32;
-        return (        
-        <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(farenheit)}</span>
-        <span className="unit"><a href="/" onClick={showCelsius}>℃</a> | ℉ </span>
-        </div>
-        );
-    }
+
 }
